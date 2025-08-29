166f9dfde05e1488b297b3041f0d373e4d09a37c
David Smitmanis <davidsm@axis.com>
2025-08-29 12:06:30 +0200

    Add playback example

--- example/src/client/vite-env.d.ts
+++ example/src/client/vite-env.d.ts
@@ -11,6 +11,11 @@ interface ImportMetaEnv {
 	readonly VITE_VIDEO_ORG_ID: string;
 	readonly VITE_VIDEO_TARGET_ID: string;
 	readonly VITE_VIDEO_SIGNALING_URL: string;
+	readonly VITE_EXAMPLE_TYPE: "live" | "playback";
+	readonly VITE_PLAYBACK_RECORDING_ID: string;
+	readonly VITE_PLAYBACK_DISK_ID: string;
+	readonly VITE_PLAYBACK_START_TIME: string;
+	readonly VITE_PLAYBACK_DURATION_SECONDS: number;
 }
 
 interface ImportMeta {
